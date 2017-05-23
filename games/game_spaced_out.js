7b7186c6b84a52ce88824aa6f86265c14926461a
Gabriel <XyPhoGR@gmail.com>
2017-05-23 04:09:42 -0500

    makin progress

--- games/game_spaced_out.js
+++ games/game_spaced_out.js
@@ -2,18 +2,23 @@ var KEY_LEFT = 37;
 var KEY_RIGHT = 39;
 var KEY_SPACE = 32;
 var KEY_P = 112;
+var KEY_R = 114;
 
 var KEY_PAUSE = KEY_P;
 var KEY_LAUNCH = KEY_SPACE;
+var KEY_RESET = KEY_R;
 
 var MS_PER_TICK = 10;
 
 var prevTickMs = new Date().getTime();
 
 var GAME_PAUSED = true;
+var GAME_OVER = false;
 
-var BALL_DX_MIN = 0.05;
-var BALL_DX_MAX = 0.85;
+var BALL_DX_MIN = 0.00;
+var BALL_DX_MAX = 0.99;
+var BALL_BOUNCE_MAX_AGE = 25;
+var BALLS_PER_GAME = 3;
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
   //window
@@ -52,6 +57,8 @@ var ballRadiusDefault = 0;
 var ballRadiusMultiplier = 1;
 
 var balls = [];
+var ballBounces = [];
+var ballsLeft = 0;
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
   //enemies
@@ -63,12 +70,14 @@ var enemies = [];
   //---
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+var gameScore = 0;
+
 function resetCanvas() {
   var oldWidth = width;
   var oldHeight = height;
 
   canvas.width = window.innerWidth - 10;
-  canvas.height = window.innerHeight - 30;
+  canvas.height = window.innerHeight - 10;
   width = canvas.width - padding * 2;
   height = canvas.height - padding * 2;
 
@@ -95,13 +104,16 @@ function resetCanvas() {
     var ball = balls[i];
     ball.x *= wMod;
     ball.y *= hMod;
-    ball.dx *= wMod;
-    ball.dy *= hMod;
   }
 
   for(var i = 0; i < enemies.length; i++) {
     var enemy = enemies[i];
     //scale boundingbox
+    var bb = enemy.bb;
+    bb.x *= wMod;
+    bb.y *= hMod;
+    bb.dx *= wMod;
+    bb.dy *= hMod;
   }
 }
 
@@ -110,14 +122,23 @@ function resetGame() {
   paddleLeft = false;
   paddleRight = false;
 
-  GAME_PAUSED = true;
+  GAME_PAUSED = false;
+  GAME_OVER = false;
 
   balls = [];
   balls.push(newBall());
+  ballsLeft = BALLS_PER_GAME - 1;
 
   enemies = [];
 
   ballRadiusMultiplier = 1;
+
+  //bb, type, lives
+  enemies.push(getEnemy(boundingBox(10, 10, 20, 20), 1, 1));
+  enemies.push(getEnemy(boundingBox(50, 30, 10, 30), 2, 2));
+  enemies.push(getEnemy(boundingBox(100, 20, 300, 100), 3, Math.floor(Math.random() * 3) + 1));
+
+  gameScore = 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -142,7 +163,10 @@ function contains(bb, x, y) {
 }
 
 function distance(bb, x, y) {
-  return 0; //TODO
+//https://stackoverflow.com/questions/5254838/calculating-distance-between-a-point-and-a-rectangular-box-nearest-point
+  var dx = Math.max(bb.x - x, 0, x - (bb.x + bb.dx));
+  var dy = Math.max(bb.y - y, 0, y - (bb.y + bb.dy));
+  return Math.sqrt(dx*dx + dy*dy);
 }
 
 //ball stuff
@@ -156,7 +180,7 @@ function getBallSpeed() {
 }
 
 function randomSpeed(ball, mag) {
-  if(!mag) mag = 0.001;
+  if(!mag) mag = 0.1;
   ball.dx += mag * Math.random() - mag * Math.random();
   ball.dy += mag * Math.random() - mag * Math.random();
 }
@@ -165,7 +189,7 @@ function newBall(x, y, dx, dy) {
   var bx = x ? x : paddleX;
   var by = y ? y : paddleY - paddleHeight / 2 - getBallRadius();
   var launched = x || y;
-  return {x: bx, y: by, dx: dx, dy: dy, launched: launched};
+  return {x: bx, y: by, dx: dx, dy: dy, launched: launched, dead: false};
 }
 
 function launchBalls() {
@@ -175,7 +199,7 @@ function launchBalls() {
   for(var i = 0; i < balls.length; i++) {
     var ball = balls[i];
     if(!ball.launched) {
-      ball.dx = dx;
+      ball.dx = dx;	
       ball.dy = dy;
       randomSpeed(ball);
       ball.launched = true;
@@ -183,20 +207,35 @@ function launchBalls() {
   }
 }
 
+function bounceBall(ball, nx, ny) {
+  ball.dx = Math.abs(ball.dx) * (nx < 0 ? -1 : 1);
+  ball.dy = Math.abs(ball.dy) * (ny < 0 ? -1 : 1);
+  ballBounces.push({x: ball.x, y: ball.y, age: 0});
+}
+
+function onBallDead(ball) {
+  //ball.y = height / 2;
+  ball.dead = true;
+}
+
 //enemy stuff
 
 function getEnemyImg(type) {
-  return "enemy_" + type;
+  return 'box';// "enemy_" + type;
 }
 
-function getEnemy(bb, type, lives) {
-  var enemy = {type: type || 1, lives: lives || 1, bb: bb, dead: false};
+function getEnemy(bb, type, lives, hitPoints, deathPoints) {
+  var enemy = {type: type || 1, lives: lives || 1, bb: bb, dead: false, hitPoints: hitPoints || 1, deathPoints: deathPoints || 0};
   enemy.onHit = enemyHitBasic.bind(enemy);
   return enemy;
 }
 
-function enemyHitBasic() {
-  if(--this.lives <= 0) this.dead = true;
+function enemyHitBasic(ball) {
+  if(this.lives > 0) gameScore += this.hitPoints;
+  if(--this.lives <= 0) {
+    if(!this.dead) gameScore += this.deathPoints;
+    this.dead = true;
+  }
 }
 
 //
@@ -232,30 +271,55 @@ function updateTick(part) {
   var ballSpeed = getBallSpeed() * part;
   for(var i = 0; i < balls.length; i++) {
     var ball = balls[i];
+    if(ball.dead) {
+      balls.splice(i, 1);
+      i--;
+      if(balls.length == 0) {
+        if(ballsLeft-- > 0) {
+          balls.push(newBall());
+        } else {
+          //end game
+          GAME_OVER = true;
+          GAME_PAUSED = true;
+        }
+      }
+    }
     if(ball.launched) {
       //test wall collision
       if(ball.x >= width - ballRadius) {
         ball.dx = -Math.abs(ball.dx);
+        ballBounces.push({x: width, y: ball.y, age: 0});
       } else if(ball.x <= ballRadius) {
         ball.dx = Math.abs(ball.dx);
+        ballBounces.push({x: 0, y: ball.y, age: 0});
       }
       if(ball.y <= ballRadius) {
         ball.dy = Math.abs(ball.dy);
+        ballBounces.push({x: ball.x, y: 0, age: 0});
       } else if(ball.y >= height + ballRadius) {
-        //dead ball
-        ball.y = height / 2; //TMP
+        //dead ball (hit floor)
+        onBallDead(ball);
       }
+
       //test paddle collision
       else if(Math.abs(ball.x - paddleX) * 2 < paddleWidth && ball.y >= paddleY - paddleHeight / 2 - ballRadius && ball.y <= paddleY) {
         ball.dy = -Math.abs(ball.dy);
         //rotate trajectory (placement and paddle dx)
-        var dist = Math.max(Math.abs(ball.x - paddleX) * 6 / paddleWidth, 0.5); //how far out it was
+        var dist = Math.max(Math.abs(ball.x - paddleX) * 5 / paddleWidth, 0.5); //how far out it was
 
         var ndx = ball.dx * dist;
 
         if(dist > 1 && Math.sign(ball.x - paddleX) != Math.sign(ndx)) {
           ndx = ball.dx - ndx;
         };
+
+        var paddleMoveMod = part * 0.1;
+        if(paddleLeft) {
+          ndx -= paddleMoveMod;
+        }
+        if(paddleRight) {
+          ndx += paddleMoveMod;
+        }
 
         if(Math.abs(ndx) > BALL_DX_MAX) {
           ndx = Math.sign(ndx) * BALL_DX_MAX;
@@ -265,8 +329,26 @@ function updateTick(part) {
 
         ball.dx = ndx;
         ball.dy = Math.sqrt(1 - ball.dx * ball.dx) * Math.sign(ball.dy);
+        ballBounces.push({x: ball.x, y: ball.y + ballRadius, age: 0});
+      } else {
+        //check corners of paddle
+        if(boundingBox(paddleX - paddleWidth / 2, paddleY - paddleHeight / 2, paddleWidth, paddleHeight).distance(ball.x, ball.y) <= ballRadius) {
+          bounceBall(ball, ball.x - paddleX, ball.y - paddleY);
+        }
+        //check side
       }
+
       //test enemy collision
+      for(var j = 0; j < enemies.length; j++) {
+        var enemy = enemies[j];
+        if(!enemy.dead && enemy.bb.distance(ball.x, ball.y) <= ballRadius) {
+          //collision
+          enemy.onHit(ball);
+          var dx = ball.x - enemy.bb.x - enemy.bb.dx / 2;
+          var dy = ball.y - enemy.bb.y - enemy.bb.dy / 2;
+          bounceBall(ball, dx, dy);
+        }
+      }
 
       ball.x += ball.dx * ballSpeed;
       ball.y += ball.dy * ballSpeed;
@@ -276,9 +358,28 @@ function updateTick(part) {
     }
   }
 
+  for(var i = 0; i < ballBounces.length; i++) {
+    var bounce = ballBounces[i];
+    bounce.age += part;
+    if(bounce.age >= BALL_BOUNCE_MAX_AGE) {
+      ballBounces.splice(i, 1);
+      i--;
+    }
+  }
+
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
   //update enemies
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  for(var i = 0; i < enemies.length; i++) {
+    var enemy = enemies[i];
+    if(enemy.dead) {
+      enemies.splice(i, 1);
+      i--;
+    } else {
+      //move and fire and whatever
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -306,10 +407,12 @@ document.addEventListener("keyup", function(e) {
 }, false);
 
 document.addEventListener("keypress", function(e) {
-  if(e.keyCode == KEY_PAUSE) {
+  if(e.keyCode == KEY_PAUSE && !GAME_OVER) {
     GAME_PAUSED = !GAME_PAUSED;
   } else if(e.keyCode == KEY_LAUNCH && !GAME_PAUSED) {
     launchBalls();
+  } else if(e.keyCode == KEY_RESET) {
+    resetGame();
   }
 }, false);
 
@@ -321,21 +424,94 @@ document.addEventListener("keypress", function(e) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function drawPaused() {
-  ctx.font = '48px sans-serif';
+  ctx.font = '64px sans-serif';
   ctx.fillStyle = "#ffffff";
+  ctx.strokeStyle = "#000000";
   ctx.textBaseline = 'top';
-  ctx.fillText('Paused', 10, 10);
-/*
-  ctx.strokeStyle = "#ff0000";
+  ctx.textAlign = 'start';
+
   ctx.lineWidth = 2;
-  ctx.strokeText('Pause', 10, 10);
-*/
+  ctx.fillText('Paused', 10, 10);
+  ctx.strokeText('Paused', 10, 10);
+
+  var x = 20;
+  var y = 70;
+  var rowHeight = 30;
+
+  ctx.font = '32px sans-serif';
+  ctx.lineWidth = 1;
+  ctx.fillText('Arrow keys to move paddle', x, y);
+  ctx.strokeText('Arrow keys to move paddle', x, y);
+  y += rowHeight;
+  ctx.fillText('Space to launch ball', x, y);
+  ctx.strokeText('Space to launch ball', x, y);
+  y += rowHeight;
+  ctx.fillText('P to unpause', x, y);
+  ctx.strokeText('P to unpause', x, y);
+  y += rowHeight;
+  ctx.fillText('R to restart', x, y);
+  ctx.strokeText('R to restart', x, y);
+}
+
+function drawGameOver() {
+  ctx.font = '64px sans-serif';
+  ctx.fillStyle = "#ffffff";
+  ctx.strokeStyle = "#000000";
+  ctx.textBaseline = 'top';
+  ctx.textAlign = 'center';
+
+  var x = width / 2;
+  var y = height / 3;
+
+  ctx.lineWidth = 2;
+  ctx.fillText('GAME OVER', x, y);
+  ctx.strokeText('GAME OVER', x, y);
+  y += 75;
+
+  ctx.font = '32px sans-serif';
+  ctx.lineWidth = 1;
+  ctx.fillText('Score: ' + gameScore, x, y);
+  ctx.strokeText('Score: ' + gameScore, x, y);
+  y = height * 2 / 3;
+  ctx.fillText('R to restart', x, y);
+  ctx.strokeText('R to restart', x, y);
+}
+
+function drawHud() {
   ctx.font = '24px sans-serif';
   ctx.fillStyle = "#ffffff";
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 1;
+
+  //draw score
+
+  //draw lives
+
+  var ballCount = balls.length;
+  var maxBallCount = BALLS_PER_GAME;
+
+  var radius = ballRadiusDefault / 2;
+  var xPadding = radius / 10;
+  var maxx = width - xPadding - radius;
+  var y = radius + 30;
+  var x = width - (radius * 2 + xPadding) * (maxBallCount - 1) - radius;
+
   ctx.textBaseline = 'top';
-  ctx.fillText('Arrow keys to move paddle', 15, 60);
-  ctx.fillText('Space to launch ball', 15, 90);
-  ctx.fillText('P to unpause', 15, 120);
+  ctx.textAlign = 'end';
+  ctx.fillText('Score: ' + gameScore, width - radius, 0);
+
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'end';
+  ctx.fillText('Lives:', x - radius - xPadding, y);
+  for(var i = 0; i < ballsLeft + 1; i++) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI*2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    x += radius * 2 + xPadding;
+  }
 }
 
 function drawPaddle() {
@@ -350,6 +526,8 @@ function drawPaddle() {
   ctx.stroke();
   ctx.closePath();
 }
+
+//
 
 function drawBalls() {
   var radius = getBallRadius();
@@ -367,6 +545,36 @@ function drawBall(ball, radius) {
   ctx.lineWidth = 1;
   ctx.stroke();
   ctx.closePath();
+}
+
+function drawBounces() {
+  var alpha = ctx.globalAlpha;
+  for(var i = 0; i < ballBounces.length; i++) {
+    var bounce = ballBounces[i];
+    ctx.beginPath();
+    ctx.globalAlpha = 1 - (bounce.age / BALL_BOUNCE_MAX_AGE);
+    ctx.arc(bounce.x, bounce.y, bounce.age / BALL_BOUNCE_MAX_AGE * getBallRadius() * 3, 0, Math.PI*2);
+    ctx.fillStyle = "#ffffff";
+    ctx.fill();
+    ctx.closePath();
+  }
+  ctx.globalAlpha = alpha;
+}
+
+//
+
+function drawEnemies() {
+  for(var i = 0; i < enemies.length; i++) {
+    var enemy = enemies[i];
+    if(!enemy.dead) {
+      drawEnemy(enemy);
+    }
+  }
+}
+
+function drawEnemy(enemy) {
+  var bb = enemy.bb;
+  ctx.drawImage(document.getElementById(getEnemyImg(enemy.type)), bb.x, bb.y, bb.dx, bb.dy);
 }
 
 //
@@ -426,11 +634,27 @@ function draw() {
   ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.translate(padding, padding);
 
+  drawBounces();
   drawPaddle();
   drawBalls();
+  drawEnemies();
+
+  drawHud();
 
   if(GAME_PAUSED) {
-    drawPaused();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    var alpha = ctx.globalAlpha;
+    ctx.globalAlpha = 0.7;
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.rect(0, 0, width + padding * 2, height + padding * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.globalAlpha = alpha;
+    ctx.translate(padding, padding);
+
+    if(GAME_OVER) drawGameOver();
+    else drawPaused();
   }
 
   prevTickMs = ms;
@@ -446,4 +670,5 @@ function draw() {
 
 resetCanvas();
 resetGame();
+GAME_PAUSED = true;
 draw();
